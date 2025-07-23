9943484c0429c82086bba29e978ac456a5f6625e
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-07-23 04:21:10 +0300

    original project

--- public/news.php
+++ public/news.php
@@ -186,7 +186,7 @@ include 'includes/header.php';
                     <div class="col-lg-8 mx-auto">
                         <?php if ($news_article['featured_image']): ?>
                             <div class="article-image mb-5">
-                                <img src="<?php echo UPLOADS_URL . '/news/' . $news_article['featured_image']; ?>"
+                                <img src="<?php echo SITE_URL; ?>/<?php echo $news['featured_image']; ?>"
                                     alt="<?php echo htmlspecialchars($news_article['title']); ?>"
                                     class="img-fluid rounded shadow">
                             </div>
